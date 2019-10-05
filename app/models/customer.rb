7ec946f1581e3dd7abb5d772efb8f3cc5db24dcc
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2019-10-04 18:36:40 -0600

    Add customers relationship endpoints

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,15 @@ class Customer < ApplicationRecord
   joins(:invoices)
     .where(invoices: {id: invoice_id})
   end
+
+  def self.favorite_customer(merchant_id)
+    joins(invoices:[:merchant, :transactions]).
+    select('customers.*, COUNT(*) AS total').
+    group(:id).
+    where(invoices:{merchant_id: merchant_id}).
+    merge(Transaction.successful).
+    order(total: :desc).
+    limit(1)
+  end
+
 end
